0d00e1cb6c84e5ada3db8a6a4517e51f77acb25e
r4j35h <gailabh@gmail.com>
2018-01-12 10:34:52 -0500

    Changing compiler plugin to 1.8

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,4 +1,5 @@
 package clinic.programming.training;
+import java.util.stream.IntStream;
 
 public class Application {
     
@@ -10,5 +11,12 @@ public class Application {
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
 		Application app = new Application();
+		
+		int[] values = {4,6,2,3,8,9,1,0};
+        IntStream.of(values).forEach(value -> System.out.printf("%d ", value));
+        System.out.println(IntStream.of(values).count());
+        System.out.println(IntStream.of(values).sum());
+        System.out.println(IntStream.of(values).max());
+        
     }
 }
